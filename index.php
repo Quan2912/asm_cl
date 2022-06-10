578a2381f46cb8a70dc3d08d0dc8e6e88ea2cda4
Quan2912 <105089810+Quan2912@users.noreply.github.com>
2022-06-10 10:23:34 +0700

    sua ket noi

--- index.php
+++ index.php
@@ -89,7 +89,8 @@
 	<div class="product-group" >
 		<div class="row">            
 			<?php
-			$connect = mysqli_connect('3.132.234.157','quanvh','123@123a','toy_car');
+			//$connect = mysqli_connect('3.132.234.157','quanvh','123@123a','toy_car');
+			$connect = mysqli_connect('localhost','root','','toy');
 			$sql = "SELECT * FROM product";
 			$result = mysqli_query($connect, $sql);
 			while ($row_toy = mysqli_fetch_array($result))
